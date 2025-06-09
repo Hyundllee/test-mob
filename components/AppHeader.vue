a915ea49dcebd9800565d1596680d8fd37f7c17f
donghyunlee <azazs0000@naver.com>
2025-06-09 10:27:32 +0900

    테스트 - 43

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -13,7 +13,7 @@
       />
 
       <q-toolbar-title>
-        <h1  class="text-h6 q-ma-none" >접근성 프로젝트 테스트당당!!!ㅡ2ㅠ !!!</h1>
+        <h1  class="text-h6 q-ma-none" >접근성 프로젝트 테스트당다리!!!ㅡ2ㅠ !!!</h1>
       </q-toolbar-title>
     </q-toolbar>
   </q-header>
